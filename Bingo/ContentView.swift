7c040ea01290d5cd4c448ae46555bebead139010
mlb21009a <mlb21009a@gmail.com>
2020-08-13 23:32:46 +0900

    create project

---
+++ Bingo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bingo
+//
+//  Created by R on 2020/08/13.
+//  Copyright © 2020 マック太郎. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
